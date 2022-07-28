14543ab25d7f11a7eaf07359c52e473f5196e29b
LucasLopes <lopes.sp.lucas@gmail.com>
2022-07-28 17:16:48 -0300

    resolve requisito 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-export default class Header extends Component {
-  render() {
-    return (
-      <div>Header</div>
-    );
-  }
+export default function Header() {
+  const { email } = useSelector((state) => state.user);
+  return (
+    <div>
+      <span data-testid="total-field">0</span>
+      <span data-testid="header-currency-field">BRL</span>
+      <span data-testid="email-field">{email}</span>
+    </div>
+  );
 }
