90d15d43d8dac45edee3144ea07fccc2da86e76e
LucasLopes <lopes.sp.lucas@gmail.com>
2022-08-01 14:45:03 -0300

    resolve requisito 7

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,8 +1,31 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function Table() {
+  const { expenses } = useSelector((state) => state.wallet);
+
+  const tableGenerator = () => {
+    const TEN = 10;
+    const resultado = expenses.map((expense) => (
+      <tr key={ expense.id }>
+        <td>{expense.description}</td>
+        <td>{expense.tag}</td>
+        <td>{expense.method}</td>
+        <td>{parseFloat(expense.value, TEN).toFixed(2)}</td>
+        <td>{expense.exchangeRates[expense.currency].name}</td>
+        <td>{parseFloat(expense.exchangeRates[expense.currency].ask, TEN).toFixed(2)}</td>
+        <td>
+          {(parseFloat(expense.value, TEN)
+      * parseFloat(expense.exchangeRates[(expense.currency)].ask, TEN)).toFixed(2)}
+        </td>
+        <td>Real</td>
+      </tr>
+    ));
+    return resultado;
+  };
+
   return (
-    <table>
+    <table border="1">
       <thead>
         <tr>
           <th>Descrição</th>
@@ -16,6 +39,9 @@ export default function Table() {
           <th>Editar/Excluir</th>
         </tr>
       </thead>
+      <tbody>
+        { expenses.length === 0 ? (<tr><td>Empty</td></tr>) : tableGenerator() }
+      </tbody>
     </table>
   );
 }
