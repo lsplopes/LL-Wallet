21e3f9b4bfea7168f0b12286abb5eebe2a406b39
LucasLopes <lopes.sp.lucas@gmail.com>
2022-08-01 14:02:36 -0300

    resolve requisito 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from '../components/Header';
+import Table from '../components/Table';
 import WalletForm from '../components/WalletForm';
 
 export default function Wallet() {
@@ -7,6 +8,7 @@ export default function Wallet() {
     <div>
       <Header />
       <WalletForm />
+      <Table />
     </div>
   );
 }
